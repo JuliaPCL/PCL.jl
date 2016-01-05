7a01c4964c4ae1ed82fcff33e0b07ba697aeac66
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-01-06 01:39:15 +0900

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,18 +5,40 @@ using Base.Test
 milk_cartoon_path = joinpath(dirname(@__FILE__), "data",
     "milk_cartoon_all_small_clorox.pcd")
 
-@testset "pcl::common" begin
+@testset "Point types" begin
+    p = pcl.PointXYZ()
+    @test icxx"$p.x;" == 0.0f0
+    @test icxx"$p.y;" == 0.0f0
+    @test icxx"$p.z;" == 0.0f0
+
+    p = pcl.PointXYZI()
+    @test icxx"$p.intensity;" == 0.0f0
+
+    p = pcl.PointXYZRGBA()
+    @test icxx"$p.r;" == 0.0f0
+    @test icxx"$p.g;" == 0.0f0
+    @test icxx"$p.b;" == 0.0f0
+    @test icxx"$p.a;" == 0xff
+end
+
+@testset "PoindCloud" begin
     cloudxyz = pcl.PointCloud{pcl.PointXYZ}()
     @test (@cxx (cloudxyz.handle)->get()) != C_NULL
     cloudxyzi = pcl.PointCloud{pcl.PointXYZI}()
     @test (@cxx (cloudxyzi.handle)->get()) != C_NULL
 
-    milk_cloud = pcl.PointCloud{pcl.PointXYZ}(milk_cartoon_path)
+    milk_cloud = pcl.PointCloud{pcl.PointXYZRGBA}(milk_cartoon_path)
     @test (@cxx (milk_cloud.handle)->get()) != C_NULL
 
     @test pcl.width(milk_cloud) == 640
     @test pcl.height(milk_cloud) == 480
     @test !pcl.is_dense(milk_cloud)
+
+    points = pcl.points(milk_cloud)
+    @test length(points) == 640 * 480
+    @test isa(points, pcl.StdVector)
+    @test isa(points[1], pcl.PointXYZRGBARef)
+    @test isa(points[1], pcl.PointXYZRGBAValOrRef)
 end
 
 @testset "pcl::io" begin
