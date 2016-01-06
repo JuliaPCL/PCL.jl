b7158911132387adb8f1f7d19391f90547543899
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-01-07 00:45:13 +0900

    pcl::Correspondences, pcl::transformPointCloud

--- src/common.jl
+++ src/common.jl
@@ -1,6 +1,8 @@
 ### common ###
 
-import Base: call, eltype, length, size, getindex
+import Base: call, eltype, length, size, getindex, push!
+
+typealias SharedPtr{T} cxxt"boost::shared_ptr<$T>"
 
 ### PointType definitions ###
 
@@ -67,3 +69,37 @@ width(cloud::PointCloud) = convert(Int, icxx"$(cloud.handle)->width;")
 height(cloud::PointCloud) = convert(Int, icxx"$(cloud.handle)->height;")
 is_dense(cloud::PointCloud) = icxx"$(cloud.handle)->is_dense;"
 points(cloud::PointCloud) = icxx"$(cloud.handle)->points;"
+
+function transformPointCloud(cloud_in::PointCloud, cloud_out::PointCloud,
+    transform)
+    icxx"""
+        pcl::transformPointCloud(*$(cloud_in.handle), *$(cloud_out.handle),
+            $transform);
+    """
+end
+
+type Correspondence
+    handle::cxxt"pcl::Correspondence"
+end
+
+call(::Type{Correspondence}) = Correspondence(
+    icxx"pcl::Correspondence();")
+function call(::Type{Correspondence}, index_query, index_match, distance)
+    handle = icxx"pcl::Correspondence($index_query, $index_match, $distance);"
+    Correspondence(handle)
+end
+
+type Correspondences
+    handle::cxxt"boost::shared_ptr<std::vector<pcl::Correspondence,
+        Eigen::aligned_allocator<pcl::Correspondence>>>"
+end
+
+function call(::Type{Correspondences})
+    handle = icxx"""
+        boost::shared_ptr<pcl::Correspondences>(
+            new pcl::Correspondences());"""
+    Correspondences(handle)
+end
+
+length(cs::Correspondences) = convert(Int, icxx"$(cs.handle)->size();")
+push!(cs::Correspondences, c::Correspondence) = icxx"$(cs.handle)->push_back($(c.handle));"
