6ddeba8524c46cfb9bb7418b2a93557558ef7383
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-01-07 00:44:22 +0900

    widen input type

--- src/kdtree.jl
+++ src/kdtree.jl
@@ -16,8 +16,9 @@ end
 setInputCloud(us::KdTreeFLANN, cloud::PointCloud) =
     icxx"$(us.handle).get()->setInputCloud($(cloud.handle));"
 
-function nearestKSearch(flann::KdTreeFLANN, point, k::Int,
+function nearestKSearch(flann::KdTreeFLANN, point, k::Integer,
     k_indices::StdVector, k_sqr_distances::StdVector)
+    k = Cint(k)
     found_neighs = icxx"""
         $(flann.handle).get()->nearestKSearch($point, $k, $k_indices,
             $k_sqr_distances);
