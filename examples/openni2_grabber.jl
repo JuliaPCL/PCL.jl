bf726d1ce471ef0b8a7233f29c4136ce08d3fd2c
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-02-27 23:31:56 +0900

    Add openni2_grabber working example
    fixes #10

--- examples/openni2_grabber.jl
+++ examples/openni2_grabber.jl
@@ -1,64 +1,85 @@
-using PCL
 using Cxx
+using OpenNI2
+using PCL
 
-addHeaderDir("/usr/local/include/ni2/", kind=C_System)
+const ni2 = OpenNI2
 
-cxx"""
-#include <iostream>
+const w = 640
+const h = 480
+const pxfmt = ni2.PIXEL_FORMAT_DEPTH_1_MM
 
-#include <mutex>
-#include <pcl/io/openni2_grabber.h>
-"""
-
-if !isdefined(:has_SimpleOpenNIViewer)
-cxx"""
-typedef pcl::PointXYZ T;
-
-class SimpleOpenNIViewer {
-public:
-  SimpleOpenNIViewer()
-      : viewer(new pcl::visualization::PCLVisualizer("PCL OpenNI Viewer")) {}
-
-  void cloud_cb_(const pcl::PointCloud<T>::ConstPtr &cloud) {
-    if (!viewer->wasStopped()) {
-      mtx.lock();
-      viewer->updatePointCloud<T>(cloud, "openni");
-      mtx.unlock();
+function getPointXYZ(stream, deptharr)
+    w, h = size(deptharr)
+    depthvec = vec(deptharr)
+    depthp = pointer(depthvec)
+    cloud = pcl.PointCloud{pcl.PointXYZ}(w, h)
+    icxx"$(cloud.handle)->is_dense = false;"
+    pointsptr = icxx"&$(cloud.handle)->points[0];"
+    icxx"""
+    const float millimeterToMeter = 0.001f;
+    for (int ri = 0; ri < $h; ++ri) {
+        for (int ci = 0; ci < $w; ++ci) {
+            auto p = $(pointsptr) + $w * ri + ci;
+            auto z = $(depthp) + $w * ri + ci;
+            openni::CoordinateConverter::convertDepthToWorld(
+                *$(stream.handle), ci, ri, *z, &p->x, &p->y, &p->z);
+            p->x *= millimeterToMeter;
+            p->y *= millimeterToMeter;
+            p->z *= millimeterToMeter;
+        }
     }
-  }
-
-  void run() {
-    pcl::Grabber *interface = new pcl::io::OpenNI2Grabber(
-        "", pcl::io::OpenNI2Grabber::OpenNI_QVGA_30Hz,
-        pcl::io::OpenNI2Grabber::OpenNI_QVGA_30Hz);
-
-    pcl::PointCloud<T>::Ptr cloud(new pcl::PointCloud<T>);
-    viewer->addPointCloud<T>(cloud, "openni", 0);
-
-    boost::function<void(const pcl::PointCloud<T>::ConstPtr &)> f =
-        boost::bind(&SimpleOpenNIViewer::cloud_cb_, this, _1);
-
-    interface->registerCallback(f);
-
-    interface->start();
-
-    while (!viewer->wasStopped()) {
-      boost::this_thread::sleep(boost::posix_time::millisec(50));
-      mtx.lock();
-      viewer->spinOnce();
-      mtx.unlock();
-    }
-
-    interface->stop();
-  }
-
-  std::mutex mtx;
-  pcl::visualization::PCLVisualizer::Ptr viewer;
-};
-"""
+    """
+    cloud
 end
 
-const has_SimpleOpenNIViewer = true
+ni2.initialize()
+device = ni2.DevicePtr()
+ni2.open(device)
+ni2.setDepthColorSyncEnabled(device, false)
 
-# viewer = @cxxnew SimpleOpenNIViewer()
-# @cxx viewer->run()
+di = ni2.getDeviceInfo(device)
+@show ni2.getName(di)
+@show ni2.getVendor(di)
+
+depth = ni2.VideoStreamPtr()
+ni2.create(depth, device, ni2.SENSOR_DEPTH)
+ni2.setMirroringEnabled(depth, true)
+
+modes = ni2.getSupportedVideoModes(ni2.getSensorInfo(device, ni2.SENSOR_DEPTH))
+for mode in modes
+    if Int(ni2.getResolutionX(mode)) == w &&
+            Int(ni2.getResolutionY(mode)) == h &&
+            ni2.getPixelFormat(mode) == pxfmt
+        println("$w x $h, PIXEL_FORMAT_DEPTH_1_MM: video mode found")
+        ni2.setVideoMode(depth, mode)
+        break
+    end
+end
+
+ni2.start(depth)
+
+frame = ni2.VideoFrameRef()
+
+info("Prepare PCL visualizer...")
+global viewer = pcl.PCLVisualizer("pcl visualizer")
+
+while true
+    ni2.waitForAnyStream([depth])
+    ni2.readFrame(depth, frame)
+    arr = convert(Array{Cushort,2}, frame)
+    @assert size(arr) == (w, h)
+
+    cloud = getPointXYZ(depth, arr)
+    if !pcl.updatePointCloud(viewer, cloud, id="depth")
+        pcl.addPointCloud(viewer, cloud, id="depth")
+    end
+    pcl.spinOnce(viewer, 1)
+
+    rand() > 0.95 && gc(false)
+end
+
+ni2.stop(depth)
+ni2.close(device)
+ni2.shutdown()
+
+close(viewer)
