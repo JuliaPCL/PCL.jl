91de87e994aa67c5664f873938506f6432c7e48f
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-09-20 16:40:33 +0900

    Module doc

--- src/PCL.jl
+++ src/PCL.jl
@@ -1,8 +1,38 @@
 __precompile__(false)
 
+# ls -l ~/.julia/v0.6  | grep PCL | awk '{print $9}' | sort | grep -v "^PCL$"|\
+#   awk '{print "-", "["$1"]""(https://github.com/JuliaPCL/"$1".jl)"}'
+"""
+The Julia wrapper for Point Cloud Library (PCL)
+
+To simplify development and minimize dependencies, the Julia wrapper consists of
+the packages below:
+
+- [LibPCL](https://github.com/JuliaPCL/LibPCL.jl)
+- [PCLCommon](https://github.com/JuliaPCL/PCLCommon.jl)
+- [PCLFeatures](https://github.com/JuliaPCL/PCLFeatures.jl)
+- [PCLFilters](https://github.com/JuliaPCL/PCLFilters.jl)
+- [PCLIO](https://github.com/JuliaPCL/PCLIO.jl)
+- [PCLKDTree](https://github.com/JuliaPCL/PCLKDTree.jl)
+- [PCLKeyPoints](https://github.com/JuliaPCL/PCLKeyPoints.jl)
+- [PCLOctree](https://github.com/JuliaPCL/PCLOctree.jl)
+- [PCLRecognition](https://github.com/JuliaPCL/PCLRecognition.jl)
+- [PCLRegistration](https://github.com/JuliaPCL/PCLRegistration.jl)
+- [PCLSampleConsensus](https://github.com/JuliaPCL/PCLSampleConsensus.jl)
+- [PCLSearch](https://github.com/JuliaPCL/PCLSearch.jl)
+- [PCLSegmentation](https://github.com/JuliaPCL/PCLSegmentation.jl)
+- [PCLSurface](https://github.com/JuliaPCL/PCLSurface.jl)
+- [PCLTracking](https://github.com/JuliaPCL/PCLTracking.jl)
+- [PCLVisualization](https://github.com/JuliaPCL/PCLVisualization.jl)
+
+following the [PCL module structure](http://docs.pointclouds.org/trunk/modules.html)
+except for the `LibPCL.jl` which manages binary dependencies (i.e. search
+installed PCL shared libraries or build and install them if not found).
+"""
 module PCL
 
 using Reexport
+using DocStringExtensions
 
 @reexport using PCLCommon
 @reexport using PCLIO
